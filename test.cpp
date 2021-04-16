b4f17ba9e1fa9296329b91e09aba223cd33971fe
sushovanmitra <39046821+sushovanmitra@users.noreply.github.com>
2021-04-16 01:42:16 -0700

    added unique func

--- test.cpp
+++ test.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 #include<vector>
 
+template<class Iter>
+Iter find_unique(Iter first, Iter last)
+{
+	if (first == last)
+		return last;
+	Iter result = first;
+	while (++first != last) {
+		if ( *first != *result && (++result != last)) {
+			*result = std::move(*first);
+        }
+    }
+	return ++result;
+}
+
 int main()
 {
 	std::vector<int> vec = {1,1,2,2,2,2,3,4,4,5,5,5,6};
-	for(auto v : vec) { std::cout<<v<<std::endl; }
+	auto last = find_unique(vec.begin(), vec.end());
+	vec.erase(last, vec.end());
+	for(auto v : vec) {
+		std::cout<<v<<std::endl;
+	}
 	return 0;
 }
